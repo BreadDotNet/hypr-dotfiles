1edeb915df8f90cb82060a3f607e8c59c9002821
Aleksei Ermola <aleksei.ermola.143@gmail.com>
2025-03-15 13:53:53 +0600

    update nvim config

---
+++ nvim/.config/nvim/lua/configs/flutter.lua
@@ -0,0 +1,3 @@
+require("flutter-tools").setup {
+  -- flutter_path = "/usr/lib/flutter/bin/flutter",
+} -- use defaults
---
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -0,0 +1,49 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre",
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "nvim-flutter/flutter-tools.nvim",
+    lazy = false,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "stevearc/dressing.nvim", -- optional for vim.ui.select
+    },
+    config = true,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "go",
+        "dart",
+      },
+    },
+  },
+
+  {
+    "kylechui/nvim-surround",
+    version = "*",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup {}
+    end,
+  },
+}
