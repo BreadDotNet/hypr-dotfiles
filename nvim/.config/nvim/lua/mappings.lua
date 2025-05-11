9eccd9624e2b28d066929bdb4b7944865358142d
Aleksei Ermola <aleksei.ermola.143@gmail.com>
2025-05-11 18:59:03 +0600

    fix nvim setup for new nvchad mappings

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -8,16 +8,3 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 -- map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
-
--- Add LSP code action mappings
-map("n", "<leader>ca", function()
-  vim.lsp.buf.code_action()
-end, { desc = "LSP Code Action" })
-
-map("v", "<leader>ca", function()
-  vim.lsp.buf.range_code_action()
-end, { desc = "LSP Range Code Action" })
-
-map("n", "<leader>sr", function()
-  vim.lsp.buf.references()
-end, { desc = "Show references" })
