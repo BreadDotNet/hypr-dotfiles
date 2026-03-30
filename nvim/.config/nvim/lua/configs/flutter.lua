cb38861e381b762bed482a83341996c16ff2b97b
Aleksei Ermola <aleksei.ermola.143@gmail.com>
2026-03-30 09:44:11 +0600

    update nvim conf from nvchad to kickstart template

--- nvim/.config/nvim/lua/configs/flutter.lua
+++
@@ -1,9 +0,0 @@
-local nvlsp = require "nvchad.configs.lspconfig"
-
-require("flutter-tools").setup {
-  flutter_path = "~/development/flutter/bin",
-  lsp = {
-    on_attach = nvlsp.on_attach,
-    capabilities = nvlsp.capabilities,
-  },
-} -- use defaults
